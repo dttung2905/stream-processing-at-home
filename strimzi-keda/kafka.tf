71036b5cdf071a18e7ca6bbfcb94f2dce9c0d3a3
dttung2905 <ttdao.2015@accountancy.smu.edu.sg>
2024-01-04 23:48:31 +0000

    Add further dependencies
    Signed-off-by: dttung2905 <ttdao.2015@accountancy.smu.edu.sg>

--- strimzi-keda/kafka.tf
+++ strimzi-keda/kafka.tf
@@ -17,17 +17,17 @@ resource "kubectl_manifest" "kafka_cluster" {
 }
 
 resource "kubectl_manifest" "kafka-consumer-app" {
-  depends_on = [kubectl_manifest.kafka_cluster]
+  depends_on = [kubectl_manifest.kafka_cluster, kubectl_manifest.kafka-topic-my-topic]
   yaml_body = file("${path.module}/manifest/kafka_consumer.yaml")
 }
 
-#resource "kubectl_manifest" "kafka-producer-job" {
-#  depends_on = [kubectl_manifest.kafka_cluster]
-#  yaml_body = file("${path.module}/manifest/kafka_producer.yaml")
-#}
+resource "kubectl_manifest" "kafka-producer-job" {
+  depends_on = [kubectl_manifest.kafka_cluster, kubectl_manifest.kafka-topic-my-topic]
+  yaml_body = file("${path.module}/manifest/kafka_producer.yaml")
+}
 
-#resource "kubectl_manifest" "kafka-topic-my-topic" {
-#  depends_on = [kubectl_manifest.kafka_cluster]
-#  yaml_body = file("${path.module}/manifest/kafka-topic.yaml")
-#}
+resource "kubectl_manifest" "kafka-topic-my-topic" {
+  depends_on = [kubectl_manifest.kafka_cluster]
+  yaml_body = file("${path.module}/manifest/kafka-topic.yaml")
+}
 
